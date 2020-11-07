642533af3de492da957255d977b03fba0fadec68
scott-22 <scotthao65@gmail.com>
2020-11-07 14:22:15 -0500

    attempting to get login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const {Client} = require('cassandra-driver');
+const appinfo = require('./properties.json');
+
+const client = new Client({
+  cloud: { secureConnectBundle: 'secure-connect-hackphs.zip' },
+  credentials: { username: app.username, password: app.password }
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${appinfo.clientId}`);
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const {Client} = require('cassandra-driver');
-const databaseinfo = require('./properties.json');
+const appinfo = require('./properties.json');
 
 const client = new Client({
   cloud: { secureConnectBundle: 'secure-connect-hackphs.zip' },
-  credentials: { username: databaseinfo.username, password: databaseinfo.password }
+  credentials: { username: app.username, password: app.password }
 });
 
 /* GET users listing. */
